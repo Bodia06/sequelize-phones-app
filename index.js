a91d7e443faf6f271861f82e6d9a2f407f9309f0
Bohdan <bohdan.verbetskyi@gmail.com>
2025-10-27 13:22:49 +0100

    Add prettier config

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http')
-const app = require('./app')
+const http = require('http');
+const app = require('./app');
 
-const httpServer = http.createServer(app)
+const httpServer = http.createServer(app);
 
-const PORT = process.env.PORT || 5001
-const HOST = process.env.HOST || 'localhost'
+const PORT = process.env.PORT || 5001;
+const HOST = process.env.HOST || 'localhost';
 
 httpServer.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`)
-})
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
