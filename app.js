7214be0a8fc1d12946621657ab6ab7f8802a2204
Bohdan <bohdan.verbetskyi@gmail.com>
2025-10-30 15:42:38 +0100

    Add static folder

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = require('./routers');
 const { errorHandlers } = require('./middleware');
+const { STATIC_PATH } = require('./constants');
 
 const app = express();
 
 app.use(express.json());
+
+// Serve static files from the 'public' directory
+app.use(express.static(`${STATIC_PATH}`));
 
 app.use('/api', router);
 
