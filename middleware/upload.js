0a887ab25ad58eb46f37082e76bc2fd0e9511dab
Bohdan <bohdan.verbetskyi@gmail.com>
2025-11-03 15:37:07 +0100

    Add store and filter to multer

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,6 +2,23 @@ const multer = require('multer');
 const path = require('path');
 const { STATIC_PATH } = require('../constants');
 
-const upload = multer({ dest: path.join(STATIC_PATH, 'images') });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(STATIC_PATH, 'images'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+function fileFilter (req, file, cb) {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Only .jpeg and .png files are allowed'), false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
 
 module.exports.uploadPhoneImages = upload.single('phoneImage');
